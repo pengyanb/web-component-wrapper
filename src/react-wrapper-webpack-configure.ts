96adc3ab8055e669c21c9d478e94f3d74737c032
pengyanb <pengyanbing@msn.com>
2021-01-27 21:44:55 +1300

    Feature/seperate build folder (#1)
    * test with hooks
    
    * update
    
    * update
    
    * update
    
    * update webpack-configure
    
    * update
    
    * update style-load for development build

--- src/react-wrapper-webpack-configure.ts
+++ src/react-wrapper-webpack-configure.ts
@@ -1,45 +1,49 @@
 import webpack from "webpack";
+import { ReplaceSource } from "webpack-sources";
 import * as path from "path";
 
 const CONST_WEB_COMPONENT_ENTRY_KEY = "webComponentEntry:";
-const CONST_INLINE_STYLE_PLACEHOLDER = "/*__WebComponentInlineStyle__*/";
+const CONST_INLINE_STYLE_PLACEHOLDER = '"' + '/*__WebComponentInlineStyle__*/' + '"';
 
 const isCss = (fileName: string) => new RegExp(".css$").test(fileName);
-const isWebComponentJs = (fileName: string) =>
-  new RegExp("webComponent.*.chunk.js$").test(fileName);
 
+let webComponentEntry: string = "";
 class WebComponentStylerPlugin {
+  constructor(private extra: any) { }
   apply(compiler: webpack.Compiler) {
-    compiler.hooks.afterCompile.tap("WebComponentStylerPlugin_compiler", (compilation) => {
-      let styleString = "";
-      const styleSet = new Set();
-      Object.keys(compilation.assets).forEach(fileName => {
-        const asset = compilation.assets[fileName];
-        if (isCss(fileName) && !styleSet.has(fileName)) {
-          styleSet.add(fileName);
-          styleString += asset.source();
-        }
-      });
-      Object.keys(compilation.assets).forEach(fileName => {
-        const asset = compilation.assets[fileName];
-        if (isWebComponentJs(fileName) && Array.isArray(asset.children)) {
-          asset.children.forEach((child: any) => {
-            if (child._value && child._value.indexOf(CONST_INLINE_STYLE_PLACEHOLDER) !== -1) {
-              child._value = child._value.replace(
-                CONST_INLINE_STYLE_PLACEHOLDER,
-                styleString
-              );
-            }
+    if (this.extra.env !== "development") {
+      compiler.hooks.compilation.tap("WebComponentStylerPlugin_compilation", (compilation) => {
+        compilation.hooks.optimizeChunkAssets.tap("WebComponentStylerPlugin_optimizeChunkAssets", chunks => {
+          let styleString = "";
+          chunks.map(chunk => {
+            chunk.files.map(file => {
+              if (isCss(file)) {
+                styleString += compilation.assets[file].source();
+              }
+            });
           });
-        }
+          chunks.map(chunk => {
+            chunk.files.map(file => {
+              const source = compilation.assets[file].source();
+              if (source.includes(CONST_INLINE_STYLE_PLACEHOLDER)) {
+                compilation.updateAsset(file, function (old) {
+                  var replaceSource = new ReplaceSource(old);
+                  const start = replaceSource.source().indexOf(CONST_INLINE_STYLE_PLACEHOLDER);
+                  replaceSource.replace(start, start + CONST_INLINE_STYLE_PLACEHOLDER.length - 1, "`" + styleString + "`");
+                  return replaceSource;
+                });
+              }
+            });
+          });
+        });
       });
-    });
+    }
   }
 }
 
-export const ReactWrapperWebpackConfigure = (webpackConfig: webpack.Configuration) => {
+export const reactWrapperWebpackConfigure = (webpackConfig: webpack.Configuration, extra: any) => {
   const options = process.argv.slice(2);
-  let webComponentEntry: string = "";
+
   options.forEach(option => {
     if (option.startsWith(CONST_WEB_COMPONENT_ENTRY_KEY)) {
       webComponentEntry = option.replace(CONST_WEB_COMPONENT_ENTRY_KEY, "");
@@ -47,21 +51,63 @@ export const ReactWrapperWebpackConfigure = (webpackConfig: webpack.Configuratio
   });
 
   if (webComponentEntry !== "") {
-    if (typeof webpackConfig.entry === 'string') {
-      webpackConfig.entry = {
-        main: webpackConfig.entry,
-        webComponent: path.join(process.cwd(), webComponentEntry)
-      }
-    } else if (Array.isArray(typeof webpackConfig.entry)) {
-      webpackConfig.entry = [...(webpackConfig.entry as string[]), webComponentEntry];
-    } else if (typeof webpackConfig.entry === 'object' && webpackConfig.entry !== null) {
-      webpackConfig.entry = {
-        ...webpackConfig.entry,
-        webComponent: path.join(process.cwd(), webComponentEntry)
-      };
+    webpackConfig.entry = path.join(process.cwd(), webComponentEntry);
+    if (webpackConfig.output) {
+      webpackConfig.output.path = path.join(process.cwd(), "webcompBuild");
+      extra.paths.appBuild = webpackConfig.output.path;
     }
+    webpackConfig.plugins?.push(new WebComponentStylerPlugin(extra));
 
-    webpackConfig.plugins?.push(new WebComponentStylerPlugin());
+    if (extra.env === "development") {
+      const cssRegex = /\.css$/;
+      webpackConfig.module?.rules.forEach(ruleObject => {
+        ruleObject.oneOf?.forEach(rule => {
+          if (rule.test && Array.isArray(rule.use) && String(rule.test) === String(cssRegex)) {
+            rule.use = rule.use.map(loader => {
+              if (typeof loader === "string" && loader.includes("style-loader")) {
+                loader = {
+                  loader: loader,
+                  options: {
+                    insert: (element: Element) => {
+                      const findWebComponent = () => {
+                        const allNodes = document.getElementsByTagName('*');
+                        for (var i = 0; i < allNodes.length; i++) {
+                          if (allNodes[i].shadowRoot) {
+                            return allNodes[i]
+                          }
+                        }
+                        return null;
+                      };
+                      const injectStyleToWebComponent = (webComp: Element) => {
+                        const style = webComp.shadowRoot?.querySelector('style');
+                        if (style?.innerHTML.includes("__WebComponentInlineStyle__")) {
+                          style.innerHTML = element.innerHTML;
+                        }
+                      };
+
+                      let webComponent = findWebComponent();
+                      if (webComponent) {
+                        injectStyleToWebComponent(webComponent);
+                      } else {
+                        const observer = new MutationObserver(() => {
+                          webComponent = findWebComponent();
+                          if (webComponent) {
+                            injectStyleToWebComponent(webComponent);
+                            observer.disconnect();
+                          }
+                        });
+                        observer.observe(document, { childList: true, subtree: true });
+                      }
+                    }
+                  }
+                }
+              }
+              return loader;
+            });
+          }
+        });
+      });
+    }
   }
   return webpackConfig;
 };
